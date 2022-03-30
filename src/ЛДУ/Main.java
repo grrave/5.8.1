8b90e4296c50305517419ef7bb4efc49c7e9ba83
Vova <100154787+grrave@users.noreply.github.com>
2022-03-30 14:21:35 +0300

    Add files via upload

---
+++ src/ЛДУ/Main.java
@@ -0,0 +1,12 @@
+package ЛДУ;
+
+public class Main {
+
+    public static void main(String[] args) {int n = 50;
+
+        if (n > 60 && n < 110)
+            System.out.print("Завдання 1: Число " +n+ " належить промажку(60, 110)");
+
+        else System.out.print("Завдання 1: Число " +n+ " не належить проміжку(60, 110)");
+    }
+}
